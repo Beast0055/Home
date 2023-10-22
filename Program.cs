5f1dd95c639d60cf810d2b2f8f3838c3d5c2f789
Anny <anny@Annys-MBP.netis.cc>
2023-10-22 21:08:25 +0400

    homework

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿string CalculateSum(int n, string res = "") {
+    if(n == 1) {
+        return res += n.ToString();
+    }
+    res+= n.ToString() + " ";
+    return CalculateSum(n - 1, res);
+}
+
+Console.WriteLine(CalculateSum(5));
+
+
+
---
+++ home 1/Program.cs
@@ -0,0 +1,10 @@
+﻿int M = int.Parse(Console.ReadLine());
+int N = int.Parse(Console.ReadLine());
+int Sum(int n1 ,int n2, int sum = 0) {
+    if(n2 == n1) {
+        return sum += n2;
+    }
+    sum += n2;
+    return Sum(n1, n2 - 1, sum);
+}
+Console.WriteLine(Sum(M, N));
